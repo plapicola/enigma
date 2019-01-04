d78381ede685f57ae4a32f5f6f9bb24e228bcba2
plapicola <p.lapicola@gmail.com>
2019-01-04 10:18:56 -0700

    Add test for encryption of message without providing a key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -29,4 +29,20 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
+
+  def test_decrypting_an_encrypted_message_returns_the_original_message
+    encrypted = @enigma.encrypt("hello world", "02715", "040895")
+
+    actual = @enigma.decrypt(encrypted[:encryption], "02715", "040895")
+
+    assert_equal "hello world", actual[:decryption]
+  end
+
+  def test_encrypting_a_message_with_no_given_key_generates_a_new_key
+    encrypted = @enigma.encrypt("hello world", "040895")
+
+    actual = @enigma.decrypt(encrypted[:encryption], encrypted[:key], "040895")
+
+    assert_equal "hello world", actual[:decryption]
+  end
 end
