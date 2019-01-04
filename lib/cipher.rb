f7f8ea948f9e29ef1dddeccbadc231c71866c850
plapicola <p.lapicola@gmail.com>
2019-01-04 16:09:14 -0700

    Adds initialize to cipher

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,3 +1,7 @@
 class Cipher
 
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
+
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -7,22 +7,27 @@ class CipherTest < Minitest::Test
   end
 
   def test_it_exists
+    # skip
     assert_instance_of Cipher, @cipher
   end
 
   def test_it_can_translate_words_given_an_offset
+    skip
     assert_equal "bcdef", @cipher.encode("abcde", 1)
   end
 
   def test_translated_words_can_contain_spaces
+    skip
     assert_equal "ifmmpaxpsme", @cipher.encode("hello world", 1)
   end
 
   def test_encoded_words_are_translated_to_lower_case
+    skip
     assert_equal "ifmmpaxpsme", @cipher.encode("HELLO WORLD", 1)
   end
 
   def test_only_alphabetical_and_whitespace_is_encoded
+    skip
     assert_equal "ifmmpaxpsme?!", @cipher.encode("hello world?!", 1)
   end
 end
