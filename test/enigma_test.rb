c5895fadaebb944be0e3c48c7ae1d3cd089dbcae
plapicola <p.lapicola@gmail.com>
2019-01-04 10:09:25 -0700

    Adds tests for encrypt and decrypt methods

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,1 +1,32 @@
 require_relative 'test_helper'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt_a_message_when_given_keys_and_dates
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_a_message_when_given_keys_and_dates
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+end
