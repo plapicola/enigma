705bab80396327e6295449cba0ebf21717331d3c
plapicola <p.lapicola@gmail.com>
2019-01-04 11:41:32 -0700

    Adds parse keys method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,8 +5,12 @@ class Enigma
   end
 
   def encrypt(message, key, date)
+
+  end
+
+  def parse_keys(key)
     keys = key.split("").each_cons(2)
-    keys = keys.map {|key| key.join("").to_i}
+    keys.map {|key| key.join("").to_i}
   end
 
 end
