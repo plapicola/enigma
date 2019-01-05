d279b2764276eed717c12e9995abe0abd36c3819
plapicola <p.lapicola@gmail.com>
2019-01-05 15:20:32 -0700

    Adds next_key method

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -6,7 +6,14 @@ class Cracker
 
   def initialize(date)
     @date = date
+    @current_key = "00000"
     @cipher = EnigmaCipher.new
+  end
+
+  def next_key(key = @current_key)
+    new_key = (key.to_i + 1).digits
+    new_key << 0 until new_key.length == 5
+    @current_key = new_key.reverse.join("")
   end
 
   def crack(phrase)
