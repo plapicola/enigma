33aff2026a35c4db128b71162a8ba73e6d43c601
plapicola <p.lapicola@gmail.com>
2019-01-05 12:10:32 -0700

    Adds command line decryption file

---
+++ lib/decrypt.rb
@@ -0,0 +1,11 @@
+require_relative 'enigma'
+
+encoded_message = File.read(ARGV.first)
+destination_file = File.open(ARGV[1], "w")
+enigma = Enigma.new
+
+decrypted_message = enigma.decrypt(encoded_message, ARGV[2], ARGV[3])
+
+destination_file.write(decrypted_message[:decryption])
+
+puts "Created #{ARGV[1]} with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}."
