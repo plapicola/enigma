bf3a72c83e8e5551cdea45c82e673c02a6df2c3c
plapicola <p.lapicola@gmail.com>
2019-01-05 14:50:57 -0700

    Adds tests for date value storage and crack methods

--- test/cracker_test.rb
+++ test/cracker_test.rb
@@ -3,13 +3,19 @@ require_relative 'test_helper'
 class CrackerTest < Minitest::Test
 
   def setup
-    @cracker = Cracker.new
+    @cracker = Cracker.new("040895")
   end
 
   def test_it_exists
     assert_instance_of Cracker, @cracker
   end
 
-  
+  def test_it_has_a_date_value
+    assert_equal "040895", @cracker.date
+  end
+
+  def test_it_can_crack_a_key
+    assert_equal "02715", @cracker.crack("keder ohulwthnw")
+  end
 
 end
