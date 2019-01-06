d77c61f4f3e6e9039d22c2b5124279dd59dd84a1
plapicola <p.lapicola@gmail.com>
2019-01-05 22:57:26 -0700

    Adds tests for ability to mutate current key to next sequential key and ability to generate shifts

--- test/key_test.rb
+++ test/key_test.rb
@@ -24,4 +24,20 @@ class KeyTest < Minitest::Test
     assert_equal 5, random_key.length
   end
 
+  def test_it_can_return_the_next_sequential_key
+    skip
+    assert_equal "02716", @key.next_key
+    assert_equal "02717", @key.next_key
+  end
+
+  def test_it_can_return_the_array_of_keys
+    skip
+    assert_equal [2, 27, 71, 15], @key.parse_keys
+  end
+
+  def test_it_can_return_shifts_if_given_a_date
+    skip
+    assert_equal [3, 27, 73, 20], @key.shifts("040895")
+  end
+
 end
