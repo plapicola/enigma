ebc4e620c969b5256f99c9dbb1327f92ba4a6960
plapicola <p.lapicola@gmail.com>
2019-01-05 18:08:11 -0700

    Updates instance_variable to be key and adds attr_reader

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -2,27 +2,27 @@ require_relative 'enigma_cipher'
 
 class Cracker
 
-  attr_reader :date
+  attr_reader :date,
+              :key
 
   def initialize(date)
     @date = date
-    @current_key = "00000"
+    @key = "00000"
     @cipher = EnigmaCipher.new
   end
 
-  def next_key(key = @current_key)
+  def next_key(key = @key)
     new_key = (key.to_i + 1).digits
     new_key << 0 until new_key.length == 5
-    @current_key = new_key.reverse.join("")
+    @key = new_key.reverse.join("")
   end
 
   def crack(phrase)
     loop do
-      attempt = @cipher.decode(phrase, @current_key, @date)
+      attempt = @cipher.decode(phrase, @key, @date)
       break if attempt[-4..-1] == " end"
       next_key
     end
-    @current_key
   end
 
 end
