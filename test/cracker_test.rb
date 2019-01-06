3fcb3d294789dfa2686f0a9f391b9f53a9483763
plapicola <p.lapicola@gmail.com>
2019-01-05 18:05:19 -0700

    Adds test for key attr_reader for cracker class

--- test/cracker_test.rb
+++ test/cracker_test.rb
@@ -14,6 +14,10 @@ class CrackerTest < Minitest::Test
     assert_equal "040895", @cracker.date
   end
 
+  def test_key_is_zeroes_by_default
+    assert_equal "00000", @cracker.key
+  end
+
   def test_it_can_generate_the_next_sequential_key
     assert_equal "00002", @cracker.next_key("00001")
     assert_equal "10000", @cracker.next_key("09999")
